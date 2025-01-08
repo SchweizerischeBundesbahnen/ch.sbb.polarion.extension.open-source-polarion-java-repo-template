179edc23f79354a28ed8cfea63a671445890dd98
ariwk <adam.ruberti@sbb.ch>
2025-01-08 20:07:47 +0100

    fix: pre-commit run -a

--- src/main/java/ch/sbb/polarion/extension/repo-template-testing/rest/controller/OpenAPIInfo.java
+++ src/main/java/ch/sbb/polarion/extension/repo-template-testing/rest/controller/OpenAPIInfo.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "extension-name REST API",
+                title = "repo-template-testing REST API",
                 version = "v1",
                 description = "",
                 termsOfService = "",
